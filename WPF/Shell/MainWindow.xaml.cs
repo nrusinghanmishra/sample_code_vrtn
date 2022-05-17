23d4644ae031072f6377ecbfaab339606c09937e
nrusinghanmishra <47710793+nrusinghanmishra@users.noreply.github.com>
2022-05-17 16:36:25 -0400

    sample code

---
+++ WPF/Shell/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Utils.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+           
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void DemoDockContainer_DockOperationCompleted(object sender, DevExpress.Xpf.Docking.Base.DockOperationCompletedEventArgs e)
+        {
+
+        }
+    }
+}
